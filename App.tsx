4e0dfeb84c2cf5578c21d1c9c194ef0732cf4224
Mihir <mihirpandey.13@gmail.com>
2024-10-06 21:19:45 +0530

    chore: Update navigation and screen listeners in IndexScreen

--- App.tsx
+++ App.tsx
@@ -20,6 +20,36 @@ export default function App() {
           <Stack.Screen name="Detail" component={DetailScreen} />
           <Stack.Screen name="CreateOrEdit" component={CreateOrEditScreen} />
         </Stack.Navigator>
+        {/* <Stack.Navigator screenOptions={{ headerTitle: "Blogs" }}> // NOTE: can also do this way to avoid having to create custom header component, but mine is better!
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                  <Feather name="plus" size={30} />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="Show"
+            component={ShowScreen}
+            options={({ route, navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("Edit", { id: route.params.id })
+                  }
+                >
+                  <EvilIcons name="pencil" size={35} />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Edit" component={EditScreen} />
+        </Stack.Navigator> */}
       </ReactQueryProvider>
     </NavigationContainer>
   );
